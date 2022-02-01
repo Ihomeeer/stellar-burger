6b5ca270e317a189acdb44af7b1a3ca6d6767140
Ihomeeer <ihomeer@gmail.com>
2022-02-01 18:14:42 +0300

    feat: начало создания файловой системы

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import appStyles from './App.module.css';
+
+function App() {
+const click = () => {
+  console.log('click')
+}
+
+  return (
+    <div className={appStyles.App}>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import appHeaderStyles from './AppHeader.module.css';
+
+function AppHeader ({}) {
+  return (
+    
+  )
+}
+export default AppHeader;
