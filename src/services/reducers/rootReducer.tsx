2317d4dfc7368791afedf212c6ace39811ab5977
Mikhail Kirichkov <mihailkirichkov@yandex.ru>
2022-06-14 21:03:31 +0300

    feat: начало тестов редакса

--- src/services/reducers/rootReducer.tsx
+++ src/services/reducers/rootReducer.tsx
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { allIngredientsReducer, constructorIngredientsReducer, currentIngredientReducer, orderReducer } from './reducers';
+import { allIngredientsReducer, constructorIngredientsReducer, currentIngredientReducer, orderReducer } from './mainReducer/reducers';
 import { userReducer } from './userReducer';
 import { wsReducer } from './WSReducer';
 
